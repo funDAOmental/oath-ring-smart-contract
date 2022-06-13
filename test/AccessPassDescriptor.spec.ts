a368ec3e3e77aa33ffb5224cbf18d561e5269082
0x1NotMe <nme15500@gmail.com>
2022-06-13 12:10:34 +1000

    feat: access pass descriptor

--- test/AccessPassDescriptor.spec.ts
+++ test/AccessPassDescriptor.spec.ts
@@ -15,7 +15,40 @@ describe.only('AccessPassDescriptor TEST', async () => {
 	});
 
     it('should return base64 encoded metadata', async () => {
-        
+        const name = "Access Pass";
+		const description = "Sample description";
+		const attributes:any = []
+		const image  = "IPFS://ABCD"
+		const expected_prefix="data:application/json;base64,"
+		const result = await descriptor.genericDataURI(name, description, attributes, image);
+		const data = result.split(",");
+		const base64String = atob(data[1]);
+		const metadata = JSON.parse(base64String);
+		expect(result).to.include(expected_prefix)
+		
+		expect(metadata.name).to.equal(name);
+		expect(metadata.description).to.equal(description);
+		expect(metadata.attributes).to.deep.equal(attributes);
+		expect(metadata.image).to.deep.equal(image)
+    });
+
+	it('should return base64 encoded metadata with attributes', async () => {
+        const name = "Access Pass";
+		const description = "Sample description";
+		const attributes:any = ['test', 'value']
+		const image  = "IPFS://ABCD"
+		const expected_prefix="data:application/json;base64,"
+		const result = await descriptor.genericDataURI(name, description, attributes, image);
+		const data = result.split(",");
+		const base64String = atob(data[1]);
+		const metadata = JSON.parse(base64String);
+		expect(result).to.include(expected_prefix)
+		
+		expect(metadata.name).to.equal(name);
+		expect(metadata.description).to.equal(description);
+		expect(metadata.attributes[0].trait_type).to.equal(attributes[0]);
+		expect(metadata.attributes[0].value).to.equal(attributes[1]);
+		expect(metadata.image).to.deep.equal(image)
     });
 
 });
