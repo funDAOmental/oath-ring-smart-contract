3494d9306e58594d87acb8c93beb38efc2871536
0x1NotMe <2715331+0x1NotMe@users.noreply.github.com>
2022-12-22 14:54:56 +1100

    Origin/fix/rob review (#22)
    * Ignore all .DS_Store
    * Removing .DS_Store files
    
    * Update Oath Ring description
    * Update Oath Ring metadata
    
    Co-authored-by: Rob Morris <rob@prismatik.com.au>

--- contracts/OathRings.sol
+++ contracts/OathRings.sol
@@ -43,7 +43,7 @@ contract OathRings is IERC2981, Ownable, ERC721Enumerable {
     IOathRingsDescriptor public oathRingsDescriptor;
 
     // IPFS content hash of contract-level metadata
-    string private contractURIHash = 'ipfs://bafkreidtlc3zjymk7cjas7x22ubpyfymtm7ngsjm4dxsmyhw555r3x3h2a';
+    string private contractURIHash = 'ipfs://bafkreigbu5fdkic4bmmvwahr7q7m2dgbc5r2jfdwlkpvjyxp6zq7svqx54';
 
     // ============ ACCESS CONTROL/SANITY MODIFIERS ============
 
--- contracts/OathRingsDescriptor.sol
+++ contracts/OathRingsDescriptor.sol
@@ -9,21 +9,21 @@ contract OathRingsDescriptor is Ownable {
     string[2] public __attributes = ['Role', 'Access Pass'];
     string public __collectionPrefix = 'Oath Ring #';
 
-    string public councilImage = 'ipfs://bafybeiepqjxt5oneuao7ntzg74svotk4m455zugzsicjt4w3g2p6llhzc4';
-    string public guildImage = 'ipfs://bafybeiepqjxt5oneuao7ntzg74svotk4m455zugzsicjt4w3g2p6llhzc4';
-    string public councilAnimationUrl = 'ipfs://bafybeiepqjxt5oneuao7ntzg74svotk4m455zugzsicjt4w3g2p6llhzc4';
-    string public guildAnimationUrl = 'ipfs://bafybeiepqjxt5oneuao7ntzg74svotk4m455zugzsicjt4w3g2p6llhzc4';
+    string public councilImage = 'ipfs://bafybeia77wnwguesvfvmq6hemepm2ugk46gv3h6cowihqrqzruy6leik4i';
+    string public guildImage = 'ipfs://bafybeia77wnwguesvfvmq6hemepm2ugk46gv3h6cowihqrqzruy6leik4i';
+    string public councilAnimationUrl = 'ipfs://bafybeihvsottfphhges3wubdomvplyhbktcd2reovbyr5fmytemaadcgvy';
+    string public guildAnimationUrl = 'ipfs://bafybeihvsottfphhges3wubdomvplyhbktcd2reovbyr5fmytemaadcgvy';
     string public councilPrefix = 'Council ';
     string public councilDetails =
-        'funDAOmental is an open ecosystem working on pluralistic, cooperative coordination. '
-        'The Oath Rings are a set of 1337 NFTs that unlock access to the funDAOmental governance system, '
+        'funDAOmental is an open ecosystem working on cooperative coordination. '
+        'The Oath Rings are a set of 1337 NFTs that unlock access to the funDAOmental governance system '
         'and reward aligned contributors with a long-term interest in our work. '
         'Each of the 337 Council Oath Rings additionally provide a seat on our council, '
         'as well as unique holder benefits and deep access to our community, ecosystem, team, and work.';
     string public guildPrefix = 'Guild ';
     string public guildDetails =
-        'funDAOmental is an open ecosystem working on pluralistic, cooperative coordination. '
-        'The Oath Rings are a set of 1337 NFTs that unlock access to the funDAOmental governance system, '
+        'funDAOmental is an open ecosystem working on cooperative coordination. '
+        'The Oath Rings are a set of 1337 NFTs that unlock access to the funDAOmental governance system '
         'and reward aligned contributors with a long-term interest in our work.';
     struct TokenURIParams {
         string name;
